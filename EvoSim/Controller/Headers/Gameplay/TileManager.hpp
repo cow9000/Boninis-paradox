f1e0da5a6c5c8ba5613e53579702a5dfb0813f53
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-01-26 12:52:51 -0700

    Added Tilemanager

---
+++ EvoSim/Controller/Headers/Gameplay/TileManager.hpp
@@ -0,0 +1,18 @@
+//
+//  TileManager.h
+//  EvoSim
+//
+//  Created by Vawdrey, Derek on 1/26/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#ifndef TileManager_hpp
+#define TileManager_hpp
+
+namespace TileManager{
+    //Tiles that are able to be created
+    enum TileList{GRASS, WATER};
+    
+};
+
+#endif /* TileManager_h */
