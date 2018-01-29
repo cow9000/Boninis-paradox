318f22777e58ecdb94e2d5aad8ffd7275c140342
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-01-29 12:24:05 -0700

    Added biome class

---
+++ EvoSim/Controller/Headers/Gameplay/Biome.hpp
@@ -0,0 +1,40 @@
+//
+//  Biome.hpp
+//  EvoSim
+//
+//  Created by Vawdrey, Derek on 1/29/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#ifndef Biome_h
+#define Biome_h
+
+#include "GameManager.hpp"
+#include "Tile.hpp"
+#include <vector>
+
+class Biome{
+public:
+    
+    
+    
+    double returnPrecipitation(){ return precipitation; }
+    double returnAverageTemperature(){ return averageTemperature; }
+    
+    GameManager::BiomeType returnBiomeType(){ return biomeType; }
+    
+    
+private:
+    
+    void generateTilesInBiome();
+    
+    double precipitation;
+    double averageTemperature;
+    
+    GameManager::BiomeType biomeType;
+    
+    //Tiles in biome
+    std::vector<*Tile> tilesInBiome;
+};
+
+#endif /* Biome_h */
